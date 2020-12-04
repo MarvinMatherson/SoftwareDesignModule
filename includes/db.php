522d1bd4a1543f2bf76e2fb22d8b73bdb8a5881c
Marvin Matherson <55834622+MarvinMatherson@users.noreply.github.com>
2020-12-04 02:30:55 +0000

    WE HAVE A LOGIN SYSTEM

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,15 @@ $db_config = [
   "db_host" => "localhost",
   "db_name" => "BikeOn",
   "db_user" => "BikeOn",
-  "db_pass" => "Superpuppy7"
+  "db_pass" => "12345"
 ];
 
-$Conn = new PDO("mysql:host=".$db_config['db_host'].";dbname=".$db_config['db_name'],$db_config['db_user'],$db_config['db_pass']);
+try {
+  $Conn = new PDO("mysql:host=".$db_config['db_host'].";dbname=".$db_config['db_name'],$db_config['db_user'],$db_config['db_pass']);
+  $Conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $Conn->setAttribute(PDO::ATTR_PERSISTENT, true);
+  $Conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e) {
+  echo $e->getMessage();
+  exit();
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+function mylogin() {
+  var help = document.getElementById("exampleInputEmail1").value
+  console.log(help)
+
+document.getElementById('inputname1').innerHTML = help;
+
+}
+
+function mysignup() {
+  var help = document.getElementById("exampleInputEmail2").value
+  console.log(help)
+
+document.getElementById('inputname2').innerHTML = help;
+}
