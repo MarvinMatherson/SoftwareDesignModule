e6459cf540ca8b41827da6a1a6688d2c4703b760
Marvin Matherson <55834622+MarvinMatherson@users.noreply.github.com>
2020-12-05 03:36:22 +0000

    making form do something

--- script.js
+++ script.js
@@ -6,9 +6,27 @@ document.getElementById('inputname1').innerHTML = help;
 
 }
 
-function mysignup() {
-  var help = document.getElementById("exampleInputEmail2").value
-  console.log(help)
-
-document.getElementById('inputname2').innerHTML = help;
+function mysignup(){
+$(document).ready(function(){
+  var username = document.getElementById("exampleInputEmail2").value
+  document.getElementById('inputname2').innerHTML = username;
+  console.log(username)
+  if (username == ''){
+    document.getElementById('nowords').style.visibility = 'visible';
+    }else{
+    $('#exampleModal2').modal('show');
+    document.getElementById('nowords').style.visibility = 'hidden';
+    console.log(username)
+  }
+});
 }
+$(document).ready(function(){
+$('#textareaID').on('input propertychange', function(){
+  if(this.value.length > 10) {
+    $('#exampleModal').modal('show');
+    $("#textareaID").val($("#textareaID").val().substring(0,11));
+
+}
+})
+});
+
